5beb3580c1a1b72f8c7bad45dacd54c8f66c38b5
Magaheedj <147484809+Magaheedj@users.noreply.github.com>
2023-10-12 20:15:51 +0400

    added work with videos

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
 
 img = cv2.imread("test.jpg")
-print(img.shape)
-img = cv2.resize(img,(500,800))
-print(img.shape)
+#print(img.shape)
+img = cv2.resize(img,(5800,800))
+##print(img.shape)
 # OpenCv    работает с BGR синии , зелённый,красный.
+cv2.imshow("Result", img)
 
-
-
+cv2.waitKey(0)
 
 
